554be01e04a7af1e4a2475d793526ea4a4b33bda
Takaya Saito <takaya.saito@hi.no>
2026-07-18 16:45:48 +0200

    Add a new table to filter area section

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-dc16d11c.js',
+                path_to_root + 'searchindex-4010bf40.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
